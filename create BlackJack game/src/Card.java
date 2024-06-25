bfd3d9cb089078d7a37452ba289975c802cb2380
akimas1846 <akihiro0923092@gmail.com>
2024-06-25 12:41:17 +0900

    Cardクラスにおける概形とMainクラスにおける宣言,デバッグはなんかおかしい

--- create BlackJack game/src/Card.java
+++ create BlackJack game/src/Card.java
@@ -1,3 +1,30 @@
 public class Card {
+    private int cardsNumber;
+    private char UniqueCardsNumber;
+    Card(int c0)
+    {
+        cardsNumber = c0;
+    }
+
+    void transrate(int cardsNumber)
+    {
+        switch (cardsNumber) {
+            case 1:
+                UniqueCardsNumber = 'A';
+                break;
+    
+            case 11:
+                UniqueCardsNumber = 'J';
+                break;
+            
+            case 12:
+                UniqueCardsNumber = 'Q';
+                break;
+            
+            default:
+                UniqueCardsNumber = 'K';
+                break;
+        }
+    }
     
 }
--- create BlackJack game/src/Main.java
+++ create BlackJack game/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        Card Clover[] = new Card[13],
+                Spead[] = new Card[13],
+                Heart[] = new Card[13],
+                Diamond[] = new Card[13];
+        
+        for (int i = 0; i < 13; i++) {
+            Clover[i] = new Card(i + 1);
+            Clover[i].transrate(i);
+            Spead[i] = new Card(i + 1);
+            Spead[i].transrate(i);
+            Heart[i] = new Card(i + 1);
+            Heart[i].transrate(i);
+            Diamond[i] = new Card(i + 1);
+            Diamond[i].transrate(i);
+        }
+
+        
     }
 }
