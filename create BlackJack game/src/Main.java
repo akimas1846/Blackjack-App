7fa624d4c13657ff01cbcbc02bf9c14d7580e627
akimas1846 <akihiro0923092@gmail.com>
2024-06-28 13:31:24 +0900

    Cardクラス完成

--- create BlackJack game/src/Main.java
+++ create BlackJack game/src/Main.java
@@ -1,22 +1,14 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Card Clover[] = new Card[13],
-                Spead[] = new Card[13],
-                Heart[] = new Card[13],
-                Diamond[] = new Card[13];
-        
-        for (int i = 0; i < 13; i++) {
-            Clover[i] = new Card(i + 1);
-            Clover[i].transrate(i);
-            Spead[i] = new Card(i + 1);
-            Spead[i].transrate(i);
-            Heart[i] = new Card(i + 1);
-            Heart[i].transrate(i);
-            Diamond[i] = new Card(i + 1);
-            Diamond[i].transrate(i);
-            Clover[i].debug();
-        }
+        Card[][] deck = new Card[4][13];
 
+        String[] cardNames = { "Clover", "Spead", "Heart", "Diamond" };
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 13; i++) {
+                deck[j][i] = new Card(i + 1, cardNames[j]);
+                deck[j][i].debug();
+            }
+        }
 
     }
 }
